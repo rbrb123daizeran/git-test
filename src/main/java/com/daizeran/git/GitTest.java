5c86094386f85145e8d2f341f1055b118882fc38
daizeran <daizeran@qq.com>
2022-09-01 18:11:51 +0800

    third commit

--- src/main/java/com/daizeran/git/GitTest.java
+++ src/main/java/com/daizeran/git/GitTest.java
@@ -3,5 +3,7 @@ package com.daizeran.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git01");
+        System.out.println("hello git02");
     }
 }
